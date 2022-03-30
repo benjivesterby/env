3835e933bb58d9b13fb514c0d13f398278e2507b
Benji Vesterby <benji@devnw.com>
2022-03-30 08:41:36 -0400

    Fixed installs and added missing check call

--- env.sh
+++ env.sh
@@ -82,9 +82,13 @@ then
 		network-manager-l2tp-gnome scdaemon pcscd \
                 bolt shellcheck xclip libpam-u2f docker-ce docker-ce-cli \
                 containerd.io terraform build-essential linux-headers-generic \
-                libbpf-dev make clang llvm libelf-dev libpcap-dev wireguard \
+                make clang llvm libelf-dev libpcap-dev wireguard \
                 yubikey-luks signal-desktop
                 
+		check $?
+
+                # unable to find libbpf-dev 
+
                 sudo apt-get install -y gnome-keyring #https://github.com/microsoft/vscode-docker/issues/1515
 
 		check $?
