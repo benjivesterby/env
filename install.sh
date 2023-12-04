350a09df5bf77fd8cbb024f169f9797c2e39faf7
Benji Vesterby <benji@codepros.org>
2023-12-03 19:56:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ apt-get update
 apt-get install -y git curl
 
 # remove the subscription notice from proxmox
+# https://johnscs.com/remove-proxmox51-subscription-notice/
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
 
 git clone --branch server https://github.com/benjivesterby/env.git
