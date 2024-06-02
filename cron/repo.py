dd83fcdf1647378fb81c5aed587b14fbe747d799
Benji Vesterby <benji@devnw.com>
2024-06-02 11:39:07 -0400

    feat: update the pull py to fix empty for dir issue

--- cron/repo.py
+++ cron/repo.py
@@ -4,6 +4,9 @@ import requests
 import subprocess
 import os
 import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Pull the GitHub Personal Access Token from an environment variable
 TOKEN = os.getenv('GITHUB_PAT')
@@ -11,7 +14,7 @@ if not TOKEN:
     raise EnvironmentError("The GITHUB_PAT environment variable is not set.")
 
 # Pull the base directory path from an environment variable, with a fallback default
-BASE_DIR = os.getenv('GITHUB_REPO_BASE_DIR', '/Users/benji/src')
+BASE_DIR = os.getenv('GITHUB_REPO_BASE_DIR', '/home/benji/src')
 
 # Ensure users.json and orgs.json paths are correctly set based on BASE_DIR or another logic
 USERS_FILE = os.path.join(BASE_DIR, 'users.json')
@@ -65,13 +68,14 @@ query($login: String!, $afterCursor: String, $isFork: Boolean!) {
   }
 }
 """
-
 def run_query(query, variables):
-    request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': variables}, headers=headers)
-    if request.status_code == 200:
+    try:
+        request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': variables}, headers=headers)
+        request.raise_for_status()
         return request.json()
-    else:
-        raise Exception(f"Query failed to run by returning code of {request.status_code}. {query}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed: {e}")
+        raise
 
 def convert_ssh_to_https(ssh_url):
     # Convert SSH URL to HTTPS URL
@@ -108,19 +112,19 @@ def fetch_repositories_for_login(login, is_fork):
 def process_repository(repo, is_fork):
     name = repo['node']['name']
     ssh_url = repo['node']['sshUrl']
-    https_url = convert_ssh_to_https(ssh_url)
+    #https_url = convert_ssh_to_https(ssh_url)
     owner = repo['node']['owner']['login']
     repo_path = os.path.join(BASE_DIR, owner, "forks" if is_fork else "", name)
 
     if not os.path.exists(repo_path):
         os.makedirs(repo_path)
         print(f"Cloning {name} into {repo_path}...")
-        clone_cmd = ["git", "clone", https_url, repo_path]
+        clone_cmd = ["git", "clone", ssh_url, repo_path]
         subprocess.run(clone_cmd)
     else:
         print(f"Repository {name} already exists. Checking and updating remote URL if necessary...")
         os.chdir(repo_path)
-        subprocess.run(["git", "remote", "set-url", "origin", https_url])
+        subprocess.run(["git", "remote", "set-url", "origin", ssh_url])
         subprocess.run(["git", "pull"])
 
 def main():
